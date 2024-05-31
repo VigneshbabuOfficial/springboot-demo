f143071ee0fccca3ce8bf7c80a40ac1c47dc3db1
Vignesh babu A <vigneshofficial2020@gmail.com>
2024-05-31 17:30:29 +0530

    first commit

---
+++ src/main/java/com/sb/demo/SpringBootDemo3Application.java
@@ -0,0 +1,23 @@
+package com.sb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/sb-demo")
+public class SpringBootDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo3Application.class, args);
+	}
+	
+	@GetMapping
+	public String demoMethod() {
+		return "Hello Welcome";
+	}
+
+}
